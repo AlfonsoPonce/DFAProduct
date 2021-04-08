178d2140b23b2a1aa51fb3cf81ac08936dab1c53
AlfonsoPonce <65920001+AlfonsoPonce@users.noreply.github.com>
2021-04-09 00:48:06 +0200

    Messirve

---
+++ ProductDFA.java
@@ -0,0 +1,69 @@
+
+
+
+
+public class ProductDFA {
+
+    
+    public static void main(String[] args) {
+        char[][] automata1 = 
+        {
+            {'1','1','2','E','E','E','E','E','E','E','E','E','E','E'},
+            {'E','E','E','E','E','E','E','E','E','E','E','E','3','4'},
+            {'E','E','E','E','E','E','E','E','E','E','E','E','E','4'},
+            {'3','3','3','3','3','3','3','3','3','3','E','E','E','4'},
+            {'5','5','6','E','E','E','E','E','E','E','5','6','E','E'},
+            {'E','E','E','E','E','E','E','E','E','E','E','E','7','8'},
+            {'E','E','E','E','E','E','E','E','E','E','E','E','E','8'},
+            {'7','7','7','7','7','7','7','7','7','7','E','E','E','8'},
+            {'E','9','9','9','9','E','E','E','E','E','E','E','E','E'},
+            {'E','E','E','E','E','E','E','E','E','E','E','E','E','E'},
+            {'E','E','E','E','E','E','E','E','E','E','E','E','E','E'},
+        };
+        char[][] automata2 = 
+        {
+            {'1','1','1','1','1','1','1','1','1','1','1','1', 'E','E'},
+            {'1','1','1','1','1','1','1','1','1','1','E','E', '2','E'},
+            {'2','2','2','2','2','2','2','2','2','2','E','E', 'E','E'},
+            {'E','E','E','E','E','4','E','E','E','E','E','E', 'E','E'},
+            {'4','4','4','4','4','4','4','4','4','4','E','E', 'E','5'},
+            {'5','5','5','5','5','5','5','5','5','5','5','5', '5','5'},
+            {'E','E','E','E','E','E','E','E','E','E','E','E', 'E','E',}          
+        };
+        
+        
+        
+        int nFuncionesTransicion = automata1[0].length;
+        int nEstados1 = automata1.length, nEstados2 = automata2.length;
+        char Error = 'E';
+        String[][] producto = new String[nEstados1 * nEstados2][nFuncionesTransicion];
+        
+        for(int k = 0; k < nEstados2; k++)
+            for(int i = 0; i < nEstados1; i++)
+                for(int j = 0; j < nFuncionesTransicion; j++)
+                    producto[k*10 + i + k][j] = automata1[i][j]+"_"+automata2[k][j];
+            
+        
+        /**
+         * OUTPUT
+         */
+        for(int k = 0; k < nEstados2; k++)
+        {
+            for(int i = 0; i < nEstados1; i++)
+            {
+                if(i == 10 && k == 6) System.out.print("["+Error+","+Error+"]");
+                else if(i == 10) System.out.print("["+Error+","+k+"]");
+                else if(k == 6) System.out.print("["+i+","+Error+"]");
+                else System.out.print("["+i+","+k+"]");
+
+                for(int j = 0; j < nFuncionesTransicion; j++)
+                    System.out.print("{"+producto[k*10 + i + k][j]+"} ");
+                
+
+                System.out.println();
+            }
+        }
+        
+    }
+    
+}
